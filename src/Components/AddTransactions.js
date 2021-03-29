4317533c1989d4dd48c42ab3e52a4d8a86e13fbb
dishamehta1995 <43091075+dishamehta1995@users.noreply.github.com>
2021-03-29 16:19:32 +0530

    feature update
    1.added income expense tab
    2. added checkbox for type of transaction type

--- src/Components/AddTransactions.js
+++ src/Components/AddTransactions.js
@@ -4,6 +4,7 @@ import { GlobalContext } from '../Context/GlobalState';
 export const AddTransactions = () => {
     const[text, setText] = useState('');
     const[amount, setAmount] = useState('');
+    const[isIncome, setIsincome] = useState(false);
     const  { addTransaction } = useContext(GlobalContext);
 
     const onSubmit = (e) => {
@@ -12,45 +13,56 @@ export const AddTransactions = () => {
         const newTransaction = {
             id: Math.floor(Math.random() * 10000),
             text,
-            amount: +amount,
-            date: date.toLocaleString("en-GB", {
-                day: "numeric",
-                month: "short",
-                year: "numeric",
-                hour: "numeric",
-                minute: "2-digit"
-              })
+            amount: (isIncome) ? +amount : -amount,
+            date: date.toLocaleString('en-GB', {
+                day: 'numeric',
+                month: 'short',
+                year: 'numeric',
+                hour: 'numeric',
+                minute: '2-digit'
+            })
         }
         addTransaction(newTransaction);
         setText('');
         setAmount('');
     }
-
+    
     return (
         <>
             <h4 className='title'>Add new transaction</h4>
             <form onSubmit={onSubmit}>
-                <div className="form-control">
-                    <label htmlFor="text">Text</label>
+                <div className='form-control'>
+                    <label htmlFor='text'>Text</label>
                     <input 
-                        type="text" 
+                        type='text' 
                         value={text} 
                         onChange={(e) => setText(e.target.value)} 
-                        placeholder="Enter text..." 
+                        placeholder='Enter text...' 
                         required
                     />
                 </div>
-                <div className="form-control">
-                    <label htmlFor="amount" >Amount <span>-ve (expense), +ve (income)</span></label >
+                <div className='form-control'>
+                    <label htmlFor='amount' >Amount </label >
                     <input 
-                        type="number" 
+                        type='number' 
                         value={amount}
                         onChange={(e) => setAmount(e.target.value)} 
-                        placeholder="Enter amount..." 
+                        placeholder='Enter amount...' 
                         required
+                        min='1'
                     />
                 </div>
-                <button className="btn">Add transaction</button>
+                <div className='form-control checkbox d-flex'>
+                    <input 
+                        type='checkbox' 
+                        id='income-expense' 
+                        name='income-expense' 
+                        value='income' 
+                        onClick={(e) => setIsincome(!isIncome)}
+                    />
+                    <label htmlFor='income-expense'>Add as Income</label>
+                </div>
+                <button className='btn'>Add transaction</button>
             </form>   
         </>
     )
--- src/Components/TransactionList.js
+++ src/Components/TransactionList.js
@@ -1,21 +1,41 @@
-import React,{useContext, useEffect} from 'react'
+import React,{useContext, useEffect, useState} from 'react'
 import { GlobalContext } from '../Context/GlobalState'
 import { Transcation } from './Transaction'
 export const TransactionsList = () => {
-    const  {transactions} = useContext(GlobalContext)
+    const [transactionType,setTransactionType] = useState('all')
+    const {transactions} = useContext(GlobalContext)
+    const [transactionData,setTransactionData] = useState(transactions)
     useEffect(() => {
         localStorage.setItem('transactions',JSON.stringify(transactions))
-    }, [transactions])
+        switch (transactionType) {
+            case 'income':
+                return setTransactionData(transactions.filter((item) => item.amount > 0 ))    
+
+            case 'expense':
+                return setTransactionData(transactions.filter((item) => item.amount < 0 ))
+
+            default:
+                return setTransactionData(transactions);
+        }
+    }, [transactions,transactionType])
+
+    const filterTransaction = (e) =>{
+        setTransactionType(e.target.attributes.datatype.value)
+    }
 
     return (
         <>
-          <h3 className='title'>History</h3>
+            <ul className='d-flex tab-list' onClick={filterTransaction}>
+                <li className={transactionType === 'all' ? 'active title tab-items' : 'title tab-items'} datatype='all'>All</li>
+                <li className={transactionType === 'income' ? 'active title tab-items' : 'title tab-items'} datatype='income'>Income</li>
+                <li className={transactionType === 'expense' ? 'active title tab-items' : 'title tab-items'} datatype='expense'>Expense</li>
+            </ul>
             <ul id="list" className="list">
                 {
-                    transactions.length === 0 ? <li className='list-item'>No transaction added yet!</li> :
-                    transactions.map((transaction,i) => (<Transcation key={i} transaction={transaction}/>))
+                    transactionData.length === 0 ? <li className='list-item'>No transaction added yet!</li> :
+                    transactionData.map((transaction,i) => (<Transcation key={i} transaction={transaction} transactionType={transactionType}/>))
                 }
-            </ul>  
+            </ul>
         </>
     )
 }
